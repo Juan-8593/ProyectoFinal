c0eb408493bc5c902876cb8885d76ac88da297fb
JuanPP8593 <juan.pirir8593@gmail.com>
2025-05-10 11:31:20 -0600

    Colores mejorados paps

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";     
+$usuario = "root";       
+$contrasena = "";          
+$baseDeDatos = "JJLCARS";  
+
+
+$conexion = new mysqli($host, $usuario, $contrasena, $baseDeDatos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+$conexion->set_charset("utf8");
+?>
